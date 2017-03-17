c86def76847ab2d93c9c6d3135f7de6e3052df8a
XuTianZhu <493512132@qq.com>
2017-03-17 19:40:11 +0800

    add try & test for it

--- static/js/todolist.js
+++ static/js/todolist.js
@@ -4,28 +4,101 @@ window.onload = function init(argument) {
   console.log(argument);
 /* ======== Main ======== */
 
-  var obj = {
-    "index": 0,
-    "title": "One",
-    "detail": "detail test"
-  };
-  var objText = JSON.stringify(obj);
-  var objCopy = JSON.parse(objText,  function(key, value) {
-    // may be
-    return value;
-  });
-  console.log(objCopy);
+  // var obj = {
+  //   "index": 0,
+  //   "title": "One",
+  //   "detail": "detail test"
+  // };
+  // var objText = JSON.stringify(obj);
+  // var objCopy = JSON.parse(objText,  function(key, value) {
+  //   // may be
+  //   return value;
+  // });
+  // console.log(objCopy);
+  try {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "http://127.0.0.1:8000/todolist/api/", false);
+    // xhr.setRequestHeader('accept', 'egoist');
+    xhr.send(null);
+    console.log("xhr.responseText: " + xhr.responseText);
 
-  var list = new Array();
-  for(var i = 0; i < 99; i++) {
-    list.push(objCopy);
+    var listObj = JSON.parse(xhr.responseText);
+    var listItems = "";
+
+    for(var i = 0; i < 99; i++) {
+        listItems = listItems + "<li>" + listObj[i%2].title + "</li>";
+    }
+    // console.log(listObj);
+    // console.log(listItems);
+
+    var todo = document.getElementById('todo');
+    todo.innerHTML = listItems;
+
+    var done = document.getElementById('done');
+    done.innerHTML = listItems;
+  } catch(e) {
+    console.log("It's on Github Page");
   }
-  console.log(list);
 
+  stroll.bind( 'ul' );
 
+  // Ripple Begin -- Temporary
+  var buttonWidth = 172;
+  var buttonHeight = 62;
+  // 这边故意不用 Math.pow
+  // var Radius = Math.sqrt((buttonWidth * buttonWidth)/4.0 + (buttonHeight * buttonHeight)/4.0);
+  var RippleDuration = 10;
+  var speed = 6;
 
+  var buttons = document.getElementsByTagName('li');
 
+  function createInner(x, y) {
+    var inner = document.createElement('div');
+    inner.style.width = speed + 'px';
+    inner.style.height = speed + 'px';
+    inner.style.transform = 'scale(1)';
 
+    // console.log("e.offsetX: " + x);
+    // console.log("e.offsetY: " + y);
+
+    inner.style.left = (x - inner.offsetWidth/2) + 'px';
+    inner.style.top = (y - inner.offsetHeight/2) + 'px';
+
+    inner.scaleValue = parseInt(inner.style.transform.slice(6).slice(0, -1)) + 1;
+    return inner;
+  }
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onmousedown = function(e) {
+      var offsetX = e.offsetX;
+      var offsetY = e.offsetY;
+      var RadiusX = (this.offsetWidth - offsetX > offsetX?this.offsetWidth - offsetX:offsetX);
+      var RadiusY = (this.offsetHeight - offsetY > offsetY?this.offsetHeight - offsetY:offsetY);
+      var maxRadius = Math.sqrt(Math.pow(RadiusX, 2) + Math.pow(RadiusY, 2));
+
+      // var inner = document.createElement('div');
+      var inner = createInner(offsetX, offsetY);
+      this.appendChild(inner);
+      var that = this;
+
+      var interval = setInterval(function () {
+        inner.scaleValue = parseInt(inner.style.transform.slice(6).slice(0, -1)) + 1;
+
+        inner.style.transform = 'scale(' + inner.scaleValue + ')';
+        inner.style.opacity = (inner.offsetWidth * inner.scaleValue)/maxRadius/2 + 0.2;
+        if((inner.offsetWidth * inner.scaleValue) >= maxRadius*2 && (inner.offsetHeight * inner.scaleValue) >= maxRadius*2) {
+          clearInterval(interval);
+          var intervalClear = setInterval(function () {
+            inner.style.opacity = inner.style.opacity - speed/that.offsetWidth ;
+            if(inner.style.opacity <= 0) {
+              that.removeChild(inner);
+              clearInterval(intervalClear);
+            }
+          }, RippleDuration * 1.2);
+        }
+      }, RippleDuration);
+    };
+  }
 
 /* ======== Main End ======== */
   var stop_time = Date.now();
